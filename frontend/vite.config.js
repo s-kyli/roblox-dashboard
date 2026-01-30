db7f90b0d30787e3a7e435f85e05ecdf5f621eec
s-kyli <johnty77777@gmail.com>
2026-01-29 21:17:29 -0800

    stop duplicating three bro

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
-    dedupe: ['three', 'react', 'react-dom']
+    alias: [
+      {
+        find: /^three$/,
+        replacement: path.resolve(__dirname, 'node_modules/three')
+      }
+    ]
   },
 
   plugins: [
